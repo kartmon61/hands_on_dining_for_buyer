0388b4740b33963e7c27e89a06875067c876631f
LeeJinho <kartmon@naver.com>
2019-07-23 13:49:50 +0900

    first upload

---
+++ hands_on_dining_for_buyer/app/src/main/java/org/mv/hands_on_dining_for_buyer/MainActivity.java
@@ -0,0 +1,14 @@
+package org.mv.hands_on_dining_for_buyer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
